bfa843a43a047fc3c1fa40407bed44c0d0c98dbd
MauriceNzai <maurice.nzai@gmail.com>
2023-03-18 09:03:29 +0000

    Add user signup, login and logout views and fuctionalities

--- autolibapp/views.py
+++ autolibapp/views.py
@@ -15,14 +15,103 @@ def index(request):
     """
     Renders the index page
     """
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+def staff(request):
+    """
+    Redirects to the staff page for user to either signup or login
+    """
+    return render(request, 'staff.html')
+
+def staffsignup(request):
+    """
+    Redirects to the signup page
+    """
+    return render(request, 'staffsignup.html')
+
+def signupbackend(request):
+    """
+    Performs the  staff signup backend processes
+    """
+    if request.method =='POST':
+        uname = request.POST["uname"]
+        fname = request.POST["fname"]
+        lname = request.POST["lname"]
+        email = request.POST["email"]
+        phone = request.POST['phone']
+        password = request.POST['password']
+        userprofile = LibUser(phone = phone)
+        if request.method == 'POST':
+            try:
+                UserExists = User.objects.get(username = request.POST['uname'])
+                messages.error(request, " Username already taken, Try something else!!!")
+                return redirect("staffsignup")
+            except User.DoesNotExist:
+                if len(uname)>10:
+                    messages.error(request," Username must be max 10 characters, Please try again")
+                    return redirect("staffsignup")
+
+                if not uname.isalnum():
+                    messages.error(request," Username should only contain letters and numbers, Please try again")
+                    return redirect("staffsignup")
+
+        # create the user
+        user = User.objects.create_user(uname, email, password)
+        user.first_name = fname
+        user.last_name = lname
+        user.email = email
+        user.save()
+        userprofile.user = user
+        userprofile.save()
+        messages.success(request," Your account has been successfully created")
+        return redirect("stafflogin")
+    else:
+        return HttpResponse('404 - NOT FOUND ')
+
+def stafflogin(request):
+    """
+    Redirects to the staff login page
+    """
+    if request.session.has_key('is_logged'):
+        return redirect('dashboard')
+    return render(request,'stafflogin.html')
+
+def loginbackend(request):
+    """
+    Handles the login backend processes
+    """
+    if request.method =='POST':
+        loginuname = request.POST["loginuname"]
+        loginpassword=request.POST["loginpassword"]
+        RegisteredUser = authenticate(username=loginuname, password=loginpassword)
+        if RegisteredUser is not None:
+            dj_login(request, RegisteredUser)
+            request.session['is_logged'] = True
+            RegisteredUser = request.user.id
+            request.session["user_id"] = RegisteredUser
+            messages.success(request, " Successfully logged in")
+            return redirect('dashboard')
+        else:
+            messages.error(request," Invalid Credentials, Please try again")
+            return redirect("/")
+    return HttpResponse('404-not found')
+
+def stafflogout(request):
+    """
+    Logs user out of the session
+    """
+    del request.session['is_logged']
+    del request.session["user_id"]
+    logout(request)
+    messages.success(request, " Successfully logged out")
+    return redirect('dashboard')
 
 def dashboard(request):
     """
     Redirects to the user's dashboard
     """
     if request.session.has_key('is_logged'):
-        Book = Book.objects.all()
+        AddBook = Book.objects.all()
         return render(request,'dashboard.html',{'Book':Book})
     return redirect('stafflogin')
 
@@ -31,7 +120,7 @@ def addbook(request):
     """
     Rdirects to the addbook page
     """
-    Book = Book.objects.all()
+    AddBook = Book.objects.all()
     return render(request,'addbook.html',{'Book':Book})
 
 def deletebook(request, id):
@@ -59,19 +148,19 @@ def returnbook(request):
     """
     return render(request,'returnbook.html')
 
-def editbookdetails(request,id):
+def editbookdetails(request, id):
     """
     Redirects to the edit book dteails page or 
     login page if user not already logged in
     """
     if request.session.has_key('is_logged'):
-        Book = Book.objects.get(id=id)
+        AddBook=Book.objects.get(id = id)
         return render(request,'editdetails.html',{'Book':Book})
     return redirect('login')
 
-
-def updatedetails(request,id):
+def updatedetails(request, id):
     """
+    Updates details of a given book
     """
     if request.session.has_key('is_logged'):
 
@@ -86,11 +175,11 @@ def updatedetails(request,id):
     return redirect('login')
 
 
-def addstudent(request):
+def addmember(request):
     """
     Ridrects to add sutdent page if user is logged in
     or login page if user not already logged in
     """
     if request.session.has_key('is_logged'):
-       return render(request,'addstudent.html')
+       return render(request, 'addstudent.html')
     return redirect ('login')
