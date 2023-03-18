ab15b814d8bcb9664fe62b99a3c2a6a1c86d8c2b
MauriceNzai <maurice.nzai@gmail.com>
2023-03-18 04:53:20 +0000

    Map index url

--- autolibapp/urls.py
+++ autolibapp/urls.py
@@ -1,11 +1,10 @@
 """
 URLS for the autolibapp are included here
-
+"""
 
 from django.urls import path
 from . import views
 
 urlpatterns = [
-
+        path('', views.index, name='index'),
     ]
-    """
