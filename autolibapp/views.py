ee5b7660589f8f725e857f213731196c936eecdf
MauriceNzai <maurice.nzai@gmail.com>
2023-03-18 04:53:51 +0000

    Add index view

--- autolibapp/views.py
+++ autolibapp/views.py
@@ -1,3 +1,11 @@
+"""
+Holds all the views for the autolibapp
+"""
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """
+    Renders the index page
+    """
+    return render(request,'index.html')
