34bd0dc0f4030bf4918cbd0b813b1de120d5b4e3
MauriceNzai <maurice.nzai@gmail.com>
2023-03-18 05:06:26 +0000

    Add page redirect urls

--- autolibapp/urls.py
+++ autolibapp/urls.py
@@ -7,4 +7,12 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
+        path('dashboard/',views.dashboard,name='dashboard'),
+        path('addbook/',views.addbook,name='addbook'),
+        path('deletebook/<int:id>',views.deletebook,name='deletebook'),
+        path('bookissue/',views.bookissue,name='bookissue'),
+        path('returnbook/',views.returnbook,name='returnbook'),
+        path('editbookdetails/<int:id>',views.editbookdetails,name='editbookdetails'),
+        path('<int:id>/updatedetails/',views.updatedetails,name='updatedetails'),
+        path('addstudent/',views.addstudent,name='addstudent'),
     ]
